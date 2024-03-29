087d711d92ca7b037916954b4403ca1ab1ee85f2
Laine Hanson <lainehanson@tamu.edu>
2024-03-29 12:44:29 -0500

    stats page has date pickers

--- frontend/src/components/ManagerView/pages/StatsPage.js
+++ frontend/src/components/ManagerView/pages/StatsPage.js
@@ -1,12 +1,32 @@
-// StatsPage.js
-import React from 'react';
-//import Inventory from '../components/Inventory';
+// statspage.js
+// npm install react-datepicker
+
+// can use this to display dates: {currentDate.toDateString()} and {comparisonDate.toDateString()}
+
+
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const StatsPage = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [comparisonDate, setComparisonDate] = useState(new Date());
+
+  const handleDateChange = (date, type) => {
+    if (type === 'current') {
+      setCurrentDate(date);
+    } else if (type === 'comparison') {
+      setComparisonDate(date);
+    }
+  };
+
   return (
     <div>
       <h1>Stats Page</h1>
-      {/* <Inventory inventory={inventory} /> */}
+      <div>
+        <p>Current Date: <DatePicker selected={currentDate} onChange={(date) => handleDateChange(date, 'current')} /></p>
+        <p>Comparison Date: <DatePicker selected={comparisonDate} onChange={(date) => handleDateChange(date, 'comparison')} /></p>
+      </div>
     </div>
   );
 };
