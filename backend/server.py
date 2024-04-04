468ec26addf41b859c8f9dd5ed1d90a3a08d2729
carolina367 <43555286+carolina367@users.noreply.github.com>
2024-04-04 14:14:35 -0500

    server changes

--- backend/server.py
+++ backend/server.py
@@ -97,15 +97,15 @@ def get_orders_info():
 
 
 # API endpoint to submit an order
-@app.route("/submit_order", methods=["GET"])
+@app.route("/submit_order", methods=["POST"])
 def submit_order():
-    data = request.form
+    data = request.json
 
     name = data.get("name")
     price = data.get("price")
     date = data.get("date")
     assigned_employee = data.get("assigned_employee")
-
+    print("test print")
     try:
         cur = conn.cursor()
     except:
@@ -113,8 +113,12 @@ def submit_order():
             host="csce-315-db.engr.tamu.edu", user="csce315_902_03_user", dbname="csce315_902_03_db", password="nighthawk", port=5432
         )
         cur = conn.cursor()
+    
+    print("test print")
     query = sql.SQL("INSERT INTO orders (name, price, date, assigned_employee) VALUES (%s, %s, %s, %s);")
     cur.execute(query, (name, price, date, assigned_employee))
+
+
     conn.commit()
     cur.close()
     return jsonify(
