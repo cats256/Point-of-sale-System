6976ed667f1a9202c2559a931dd0e35afca2b783
Matthew Speights <mattcspeights@gmail.com>
2024-05-05 19:30:55 -0500

    Added navbar to manager view

--- frontend/src/components/ManagerView/ManagerView.js
+++ frontend/src/components/ManagerView/ManagerView.js
@@ -7,21 +7,41 @@ import EmployeesPage from "./pages/EmployeesPage";
 import InventoryPage from "./pages/InventoryPage";
 import StatsPage from "./pages/StatsPage";
 import MenuPage from "./pages/MenuPage";
+import NavBar from "../common/navBar";
+
 // Use named export export { component_name } instead of export default, makes it easier when changing component's name
 
 const ManagerView = () => {
     const [page, setPage] = useState("employees"); // Default page is employees
+    const [zoom, setZoom] = useState(100);
+    
+    const increaseZoom = () => {
+        setZoom(zoom + 25);
+    };
+
+    const decreaseZoom = () => {
+        if (zoom > 100) {
+            setZoom(zoom - 25);
+        }
+    };
 
     return (
-        <div className="app">
-            <Sidebar setPage={setPage} />
-            <div className="content">
-                {page === "employees" && <EmployeesPage />}
-                {page === "inventory" && <InventoryPage />}
-                {page === "stats" && <StatsPage />}
-                {page === "menu" && <MenuPage />}
+        <main style={{minHeight: '100vh'}}>
+            <NavBar
+                increaseZoom={increaseZoom}
+                decreaseZoom={decreaseZoom}
+                zoom={zoom}
+            />
+            <div className="app">
+                <Sidebar setPage={setPage} />
+                <div className="content">
+                    {page === "employees" && <EmployeesPage />}
+                    {page === "inventory" && <InventoryPage />}
+                    {page === "stats" && <StatsPage />}
+                    {page === "menu" && <MenuPage />}
+                </div>
             </div>
-        </div>
+        </main>
     );
 };
 
